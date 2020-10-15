3fbd8aa4bf1021a1ea62b474fcd48d1d478413c8
anton sjoo <anton.sjoo@hotmail.se>
2020-10-15 13:48:11 +0200

    volume

--- sketches/wrist-distance/sketch.js
+++ sketches/wrist-distance/sketch.js
@@ -3,6 +3,24 @@
   // in browsers that have a strict autoplay policy.
   // See also: userStartAudio(); ssdsd
 
+  let mySound;
+  function preload() {
+    soundFormats('mp3', 'ogg');
+    mySound = loadSound('cam.mp3');
+  }
+  
+  function setup() {
+  }
+  
+  function playSound() {
+    // playing a sound file on a user gesture
+    // is equivalent to `userStartAudio()`
+    mySound.setVolume(bodyVolume)
+    mySound.play();
+  }
+
+
+
 var posX = 0;
 var posY = 0;
 var globalDistance = 0;
@@ -14,38 +32,49 @@ ctx.scale(-1,1)
 }
 function draw(){
 
-  console.log(posX)
+  
   if (mouseIsPressed) {
     fill(0);
   } else {
     fill(255);
   }
   ellipse(posX, posY, 20, 20);
+  console.log(bodyVolume)
 }
 
 
 const bodies = new BodyStream ({
       posenet: posenet,
       architecture: modelArchitecture.MobileNetV1, 
-      detectionType: detectionType.singleBody, 
+      detectionType: detectionType.multipleBodies, 
       videoElement: document.getElementById('video'), 
       samplingRate: 250})
-    
-let body
-
+let bodyVolume;    
+let body;
+let body2
+//let body2
 bodies.addEventListener('bodiesDetected', (e) => {
   body = e.detail.bodies.getBodyAt(0)
+  body2 = e.detail.bodies.getBodyAt(1)
   const leftEye = body.getBodyPart(bodyParts.leftEye)
   const rightEye = body.getBodyPart(bodyParts.rightEye)
   
     const distance = Math.round(body.getDistanceBetweenBodyParts(bodyParts.leftEye, bodyParts.rightEye))
     globalDistance = distance;
-    document.getElementById('output').innerText = `Position: ${posX}`
+    document.getElementById('output').innerText = `Position: ${bodyVolume}`
     body.getDistanceBetweenBodyParts(bodyParts.leftEye, bodyParts.rightEye)
 
     posX = Math.round(rightEye.position.x)
-    posY = Math.round(rightEye.this.position.y)
-    
+    posY = Math.round(rightEye.position.y)
+
+    bodyVolume = posX/560;
+    if(bodyVolume > 1){
+      bodyVolume = 1;
+    }
+    else if(bodyVolume < 0){
+      bodyVolume = 0;
+    }
+playSound();
 })
 
 
